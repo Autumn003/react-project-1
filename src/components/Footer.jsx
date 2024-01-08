177459ef65235ea29eed17adbbe15e031f89a90d
Hemant <hemantsharma20031204@gmail.com>
2024-01-08 11:40:30 +0530

    initial-commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>Techstar</h1>
+            <p>@All rights reserved</p>
+        </div>
+
+        <div>
+            <h5>Follow Us</h5>
+            <div>
+                <a href="https://github.com/Autumn003" target={"blank"}>GitHub</a>
+                <a href="https://www.linkedin.com/in/hemant003/" target={"blank"}>Linkedin</a>
+                <a href="https://autumn003.github.io/Portfolio/" target={"blank"}>Portfolio</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
